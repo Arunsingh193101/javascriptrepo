a5267351796877644e15c71df3bc51eb395c7134
Arunsingh193101 <76801780+Arunsingh193101@users.noreply.github.com>
2025-01-30 16:57:04 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const bankId = 1234
+
+let bankAccount = "123def"
+bankAccount="123abc"
+
+var bankCity = "chandigarh"
+bankCity = "Mohali"
+
+bankBranch = "Mohali"
+bankBranch = "zirakpur"
+
+console.table([bankId,bankAccount,bankCity,bankBranch,])
